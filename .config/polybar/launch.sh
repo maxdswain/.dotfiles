4f2b8474139d9604a8d678f849b26510f4e33f04
Max Swain <89113255+maxdswain@users.noreply.github.com>
2024-01-02 13:52:19 +0000

    Fix polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,16 +12,16 @@ primary=$(xrandr --query | grep primary | cut -d" " -f1)
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if [ "$(echo "$screens" | wc -l)" -eq 1 ]; then
-    MONITOR=$primary polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+    MONITOR=$primary polybar main 2>&1 | tee -a /tmp/polybar1.log &
 else
     xrandr --output HDMI-1-1 --auto --right-of eDP-1
     xrandr --output HDMI-1-1 --primary
     primary=$(xrandr --query | grep primary | cut -d" " -f1)
     for m in $screens; do
         if [ $m = $primary ]; then
-            MONITOR=$m polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+            MONITOR=$m polybar main 2>&1 | tee -a /tmp/polybar1.log &
         else
-            MONITOR=$m polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+            MONITOR=$m polybar secondary 2>&1 | tee -a /tmp/polybar2.log &
         fi
     done
 fi
